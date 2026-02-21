4bc194481e57e300f97921472986be5668f86559
Navadeep <navadeep@gmail.com>
2026-02-21 04:51:54 -0500

    Added multiplayer tic tac toe

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 
 // home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "navdeep.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
